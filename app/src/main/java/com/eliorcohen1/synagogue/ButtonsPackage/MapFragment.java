8c2969aed0c1e6e2c276b098e7675cb5b3af4cdc
EliorCohen <elior123456123123@gmail.com>
2018-12-26 06:32:09 +0200

    update

--- app/src/main/java/com/eliorcohen1/synagogue/ButtonsPackage/MapFragment.java
+++ app/src/main/java/com/eliorcohen1/synagogue/ButtonsPackage/MapFragment.java
@@ -106,70 +106,9 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
             if (provider2 != null) {
                 location = locationManager.getLastKnownLocation(provider2);
                 if (location != null) {
-                    double lat = location.getLatitude();
-                    double lng = location.getLongitude();
-                    String lat1 = String.valueOf(lat);
-                    String lng1 = String.valueOf(lng);
+                    googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 8));
 
-                    //Define list to get all latLng for the route
-                    ArrayList<LatLng> path = new ArrayList<LatLng>();
-
-                    //Execute Directions API request
-                    GeoApiContext context = new GeoApiContext.Builder()
-                            .apiKey("AIzaSyBrPt88vvoPDDn_imh-RzCXl5Ha2F2LYig")
-                            .build();
-                    DirectionsApiRequest req = DirectionsApi.getDirections(context, lat1 + ", " + lng1, "31.742462, 34.985447");
-                    try {
-                        DirectionsResult res = req.await();
-
-                        //Loop through legs and steps to get encoded polyLines of each step
-                        if (res.routes != null && res.routes.length > 0) {
-                            DirectionsRoute route = res.routes[0];
-
-                            if (route.legs != null) {
-                                for (int i = 0; i < route.legs.length; i++) {
-                                    DirectionsLeg leg = route.legs[i];
-                                    if (leg.steps != null) {
-                                        for (int j = 0; j < leg.steps.length; j++) {
-                                            DirectionsStep step = leg.steps[j];
-                                            if (step.steps != null && step.steps.length > 0) {
-                                                for (int k = 0; k < step.steps.length; k++) {
-                                                    DirectionsStep step1 = step.steps[k];
-                                                    EncodedPolyline points1 = step1.polyline;
-                                                    if (points1 != null) {
-                                                        //Decode polyline and add points to list of route coordinates
-                                                        List<com.google.maps.model.LatLng> coords1 = points1.decodePath();
-                                                        for (com.google.maps.model.LatLng coord1 : coords1) {
-                                                            path.add(new LatLng(coord1.lat, coord1.lng));
-                                                        }
-                                                    }
-                                                }
-                                            } else {
-                                                EncodedPolyline points = step.polyline;
-                                                if (points != null) {
-                                                    //Decode polyline and add points to list of route coordinates
-                                                    List<com.google.maps.model.LatLng> coords = points.decodePath();
-                                                    for (com.google.maps.model.LatLng coord : coords) {
-                                                        path.add(new LatLng(coord.lat, coord.lng));
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    } catch (Exception e) {
-                        Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_LONG).show();
-                    }
-
-                    //Draw the polyline
-                    if (path.size() > 0) {
-                        PolylineOptions opts = new PolylineOptions().addAll(path).color(Color.BLUE).width(5);
-                        mGoogleMap.addPolyline(opts);
-                    }
                 }
-                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 8));
             }
         } catch (Exception e) {
 
@@ -178,7 +117,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     public void addMarker() {
         marker = new MarkerOptions().position(new LatLng(31.742462, 34.985447)).title("בית הכנסת - נווה צדק").icon(BitmapDescriptorFactory
-                .defaultMarker(BitmapDescriptorFactory.HUE_ORANGE));
+                .defaultMarker(BitmapDescriptorFactory.HUE_VIOLET));
         mGoogleMap.addMarker(marker);
         mGoogleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
             @Override
@@ -259,7 +198,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
 
                         //Draw the polyline
                         if (path.size() > 0) {
-                            PolylineOptions opts = new PolylineOptions().addAll(path).color(Color.BLUE).width(5);
+                            PolylineOptions opts = new PolylineOptions().addAll(path).color(Color.rgb(153, 153, 102)).width(5);
                             mGoogleMap.addPolyline(opts);
                         }
                     }
