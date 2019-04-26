2baaaabd1b7f92071ad8be19d90e7d585aba5ed7
EliorCohen <elior123456123123@gmail.com>
2019-04-26 20:41:18 +0300

    update

--- app/src/main/java/com/eliorcohen1/synagogue/StartPackage/MainActivity.java
+++ app/src/main/java/com/eliorcohen1/synagogue/StartPackage/MainActivity.java
@@ -202,7 +202,6 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         buildGoogleApiClient();
 
