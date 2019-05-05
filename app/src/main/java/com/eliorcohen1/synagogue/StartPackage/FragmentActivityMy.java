1e94b7c579e8941837b05ebe7ea92d2c911a93af
EliorCohen <elior123456123123@gmail.com>
2019-05-05 22:10:10 +0300

    update

--- app/src/main/java/com/eliorcohen1/synagogue/StartPackage/FragmentActivityMy.java
+++ app/src/main/java/com/eliorcohen1/synagogue/StartPackage/FragmentActivityMy.java
@@ -2,6 +2,8 @@ package com.eliorcohen1.synagogue.StartPackage;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -12,6 +14,7 @@ import com.eliorcohen1.synagogue.R;
 public class FragmentActivityMy extends AppCompatActivity {
 
     Button backMap;
+    private CoordinatorLayout coordinatorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,17 @@ public class FragmentActivityMy extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         backMap = findViewById(R.id.backMap);
+        coordinatorLayout = findViewById(R.id.myContent);
+
+        Snackbar.make(coordinatorLayout, R.string.item_removed_message, Snackbar.LENGTH_LONG)
+                .setAction(R.string.undo, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // Respond to the click, such as by undoing the modification that caused
+                        // this message to be displayed
+                    }
+                })
+                .show();
 
         backMap.setOnClickListener(new View.OnClickListener() {
             @Override
