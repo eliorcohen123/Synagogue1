31380ac601d2dceffd0eb12c4b4ede9ebe0eb792
EliorCohen <elior123456123123@gmail.com>
2019-07-14 22:16:54 +0300

    update

---
+++ app/src/main/java/com/eliorcohen1/synagogue/StartPackage/CountryData.java
@@ -0,0 +1,8 @@
+package com.eliorcohen1.synagogue.StartPackage;
+
+public class CountryData {
+
+    public static final String[] countryNames = {"ישראל"};
+
+    public static final String[] countryAreaCodes = {"972"};
+}
