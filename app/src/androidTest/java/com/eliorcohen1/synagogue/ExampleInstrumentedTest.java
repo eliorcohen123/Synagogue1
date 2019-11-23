83e44c134c835270fa32986e12876e85acfe9b1f
EliorCohen <elior123456123123@gmail.com>
2019-11-23 14:04:12 +0200

    update

--- app/src/androidTest/java/com/eliorcohen1/synagogue/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/eliorcohen1/synagogue/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.synagogue", appContext.getPackageName());
+        assertEquals("com.eliorcohen1.synagogue", appContext.getPackageName());
     }
 }
