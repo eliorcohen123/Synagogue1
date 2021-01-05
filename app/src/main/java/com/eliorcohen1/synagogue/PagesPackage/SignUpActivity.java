7dfc417e206462609410653467ad0f919cc97875
EliorCohen <elior123456123123@gmail.com>
2021-01-05 19:00:39 +0200

    update

--- app/src/main/java/com/eliorcohen1/synagogue/PagesPackage/SignUpActivity.java
+++ app/src/main/java/com/eliorcohen1/synagogue/PagesPackage/SignUpActivity.java
@@ -2,7 +2,9 @@ package com.eliorcohen1.synagogue.PagesPackage;
 
 import android.content.Intent;
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -36,9 +38,9 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
     private void initUI() {
         btnSignUp = findViewById(R.id.login_page);
         email_id = findViewById(R.id.input_email);
-        progressBar = findViewById(R.id.progressBar);
         passwordCheck = findViewById(R.id.input_password);
         myBtnSignUp = findViewById(R.id.btn_signup);
+        progressBar = findViewById(R.id.progressBar);
 
         mAuth = FirebaseAuth.getInstance();
     }
@@ -51,30 +53,34 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
     private void register() {
         String email = email_id.getText().toString();
         String password = passwordCheck.getText().toString();
+        if (!EmailPasswordPhoneValidator.getInstance().isValidEmail(email) && !EmailPasswordPhoneValidator.getInstance().isValidPassword(password)) {
+            email_id.setError("האימייל לא חוקי");
+            passwordCheck.setError("הסיסמא לא חוקית");
+            email_id.requestFocus();
+            passwordCheck.requestFocus();
+        } else if (!EmailPasswordPhoneValidator.getInstance().isValidEmail(email)) {
+            email_id.setError("האימייל לא חוקי");
+            email_id.requestFocus();
+        } else if (!EmailPasswordPhoneValidator.getInstance().isValidPassword(password)) {
+            passwordCheck.setError("הסיסמא לא חוקית");
+            passwordCheck.requestFocus();
+        } else {
+            progressBar.setVisibility(View.VISIBLE);
 
-        if (!EmailPasswordPhoneValidator.getInstance().isValidEmail(email)) {
-            Toast.makeText(getApplicationContext(), "האימייל לא חוקי", Toast.LENGTH_SHORT).show();
-            return;
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(SignUpActivity.this, task -> {
+                        progressBar.setVisibility(View.GONE);
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "createUserWithEmail:success");
+                            Intent intent1 = new Intent(SignUpActivity.this, SignInActivity.class);
+                            startActivity(intent1);
+                            finish();
+                        } else {
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SignUpActivity.this, getString(R.string.failed), Toast.LENGTH_SHORT).show();
+                        }
+                    });
         }
-        if (!EmailPasswordPhoneValidator.getInstance().isValidPassword(password)) {
-            Toast.makeText(getApplicationContext(), "הסיסמא לא חוקית", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        progressBar.setVisibility(View.VISIBLE);
-
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(SignUpActivity.this, task -> {
-                    progressBar.setVisibility(View.GONE);
-                    if (task.isSuccessful()) {
-                        Log.d(TAG, "createUserWithEmail:success");
-                        Intent intent1 = new Intent(SignUpActivity.this, SignInActivity.class);
-                        startActivity(intent1);
-                        finish();
-                    } else {
-                        Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                        Toast.makeText(SignUpActivity.this, getString(R.string.failed), Toast.LENGTH_SHORT).show();
-                    }
-                });
     }
 
     @Override
